b00b1d6cf35bba07bfbe6ee15a0780bf1d542f73
Dražen Šarić <80947246+Drazo420@users.noreply.github.com>
2022-05-16 12:34:21 +0200

    Update and rename Dust_Setup.ino to LDS_Setup.ino

---
+++ LDS_Setup.ino
@@ -0,0 +1,29 @@
+char dustMsg[30];
+
+int LDS_PM25 = 0;
+
+void LDS_read() {
+
+  if ( Serial1.read() == 0x42 && Serial1.read() == 0x4D ) {     // When starting bits are detected, start reading
+
+    int msgIdx = 0;
+    for ( int i = 0; i < 30; i++ ) {
+
+      while ( !Serial1.available() );
+
+      dustMsg[msgIdx++] = Serial1.read();
+
+    }
+
+    LDS_PM25 = (dustMsg[4] * 256) + dustMsg[5];
+
+    Serial.println("");
+    Serial.print("PM2.5 = ");
+    Serial.print(LDS_PM25);
+    Serial.println(" ug/m3");
+    Serial.println("");
+
+    store_PM25(LDS_PM25);
+
+  }
+}
