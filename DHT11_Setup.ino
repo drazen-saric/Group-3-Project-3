e67b2d90ab10a572c122cc1568be89445587a4cc
Dražen Šarić <80947246+Drazo420@users.noreply.github.com>
2022-05-16 12:33:45 +0200

    Update DHT11_Setup.ino

--- DHT11_Setup.ino
+++ DHT11_Setup.ino
@@ -1,37 +1,36 @@
 #include <DHT.h>
 
-//Constants
-
 #define DHTTYPE DHT22   // DHT22
 
-DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
+//Value variables
+float hum = 0.0;  //Stores humidity value
+float temp = 0.0; //Stores temperature value
 
-//Variables
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
+DHT dht(DHTPIN, DHTTYPE);
 
-
-void setup_DHT(){
+void setup_DHT() {
   dht.begin();
 }
 
-void DHT_read(){
+void DHT_read() {
   float converted = 0.00;
 
-    digitalWrite(DHTPWR, HIGH);
-    delay(2000);
-    
-    //Read data and store it to variables hum and temp
-    hum = dht.readHumidity();
-    temp= dht.readTemperature();
+  //Read sensor and store it to variables hum and temp
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
 
-    digitalWrite(DHTPWR, LOW);
 
-    Serial.print("Celsius = ");
-    Serial.print(temp);
-    Serial.println(" C");
+  Serial.print("Temperature = ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println("");
 
-    Serial.print("Humidity =");
-    Serial.println(hum);
-    
+  Serial.print("Humidity = ");
+  Serial.println(hum);
+  Serial.println("");
+
+  store_Temp(temp);
+  store_Humid(hum);
+
 }
+
