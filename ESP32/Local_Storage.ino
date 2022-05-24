16b7d25b902c883e913572eda8c8308de123dca6
Dražen Šarić <80947246+drazen-saric@users.noreply.github.com>
2022-05-24 13:03:24 +0200

    Update Local_Storage.ino
    Code updated to send in timestamp order instead of pointer order

--- ESP32/Local_Storage.ino
+++ ESP32/Local_Storage.ino
@@ -13,6 +13,7 @@ const char* MAC_Topic = "group3/22/mac";
 
 //Counters used for sending the data in a proper order
 int pointer = 0;
+int pointerLimit = 0;
 int round_count = 0;
 
 //Functions store the measured values
@@ -54,10 +55,14 @@ void increment_pointer() {
 
 void data_send() {
   if (round_count == 1) {
-    pointer = 99;
+    pointer = 0;
+    pointerLimit = 99;
   }
 
-  while (pointer != -1) {
+  pointerLimit = pointer;
+  pointer = 0;
+
+  while (pointer <= pointerLimit) {
 
     Serial.print("Sent data on pointer nr. ");
     Serial.println(pointer);
@@ -66,27 +71,27 @@ void data_send() {
     Serial.print("Sending value for Temperature: ");
     Serial.println(Temp_Values[pointer]);
     delay(500);
-    
+
     DataToMsg(Data_Topic, Humid_Values[pointer]);
     Serial.print("Sending value for Humidity: ");
     Serial.println(Humid_Values[pointer]);
     delay(500);
-    
+
     DataToMsg(Data_Topic, Light_Values[pointer]);
     Serial.print("Sending value for Light: ");
     Serial.println(Light_Values[pointer]);
     delay(500);
-    
+
     DataToMsg(Data_Topic, PM25_Values[pointer]);
     Serial.print("Sending value for PM25: ");
     Serial.println(PM25_Values[pointer]);
     delay(500);
-    
+
     PublishMsg(Data_Topic, Time_Values[pointer]);
     Serial.print("Sending value for Time: ");
     Serial.println(Time_Values[pointer]);
     delay(500);
-    
+
     PublishMsg(MAC_Topic, MAC_Values[pointer]);
     Serial.print("Sending value for MAC: ");
     Serial.println(MAC_Values[pointer]);
@@ -94,13 +99,14 @@ void data_send() {
 
     Serial.println("");
 
-    pointer -= 1;
+    pointer += 1;
 
   }
   //Set pointer and round_count to 0
-  increment_pointer();
+  pointer = 0;
+  pointerLimit = 0;
   round_count = 0;
   Serial.println("Complete send!");
   PublishMsg("group3/22/sync", "Complete data set sent to mqtt");
-  
+
 }
