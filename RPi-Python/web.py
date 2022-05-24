49dd99c22ddaf5db7754695cdd739c0fedb7d6ca
fae0lure <104417841+fae0lure@users.noreply.github.com>
2022-05-24 13:01:45 +0200

    Update web.py

--- RPi-Python/web.py
+++ RPi-Python/web.py
@@ -1,11 +1,14 @@
-import mariadb, sys
+import mariadb
+import  sys
 from flask import Flask, render_template
 
-heading = ['Room number', 'Temperature','Humidity','Light','Dust','Time']
+#static list of table headings
+heading = ['Room Number', 'Temperature','Humidity','Light','Dust','Time']
 
 app = Flask(__name__)
 
 @app.route('/')
+#mariadb connect
 def dyna():
     try:
         conn = mariadb.connect(
@@ -18,10 +21,15 @@ def dyna():
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
         sys.exit(1)
+
+#dynamic list of table data
     datasheet = []
+#query and appending to list
     cur = conn.cursor()
-    cur.execute("SELECT Temperature_Celsius, Humidity, Light, Dust, Time_stamps FROM room ORDER BY id DESC LIMIT 10;")
+    cur.execute("SELECT Room_number, Temperature_Celsius, Humidity, Light, Dust, Time_stamps FROM room ORDER BY id DESC LIMIT 10;")
     for m in cur:
         datasheet.append(m)
+#render html template with list attributes
     return render_template("webface.html", data=datasheet, headings=heading)
+#run app
 app.run(host='0.0.0.0', port=5000)
